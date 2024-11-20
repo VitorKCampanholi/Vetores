98ed51f1e825231031fb770fcd4c6174b4896132
VitorKCampanholi <adegawhc@outlook.com>
2024-11-20 00:50:06 -0300

    Adicionar arquivos de projeto.

---
+++ Vetores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+
+
+int n = int.Parse(Console.ReadLine());
+
+double[] vect = new double[n];
+
+for (int  i = 0;  i < n; i++)
+{
+    vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+}
+
+double soma = 0.0;
+for (int i = 0; i < n; i ++) 
+{
+    soma += vect[i];
+}
+double avg = soma / n;
+
+Console.WriteLine("VERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
